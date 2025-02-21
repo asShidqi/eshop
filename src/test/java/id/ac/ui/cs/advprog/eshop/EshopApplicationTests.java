f980e8f549377fd7f57288829c9b66ce5b2a49fe
asShidqi <fayyed76@gmail.com>
2025-02-21 20:02:18 +0700

    add test for Eshop main

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,24 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testMain() {
+        try (var mockedSpringApplication = mockStatic(SpringApplication.class)) {
+            EshopApplication.main(new String[]{});
+            mockedSpringApplication.verify(() -> SpringApplication.run(EshopApplication.class, new String[]{}));
+        }
     }
 
 }
