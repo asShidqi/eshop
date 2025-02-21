fb1b11af0e2893cb3f3fd20d6b315f48d09704d6
asShidqi <fayyed76@gmail.com>
2025-02-21 20:41:09 +0700

    memperbaiki file line problem dari code analysis PMD

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
